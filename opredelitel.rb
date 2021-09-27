498365e6e4e763b5a1910af5310ebf87ea8a7024
Karam Sadykhov <karam.sadykhov@onlinesup.com>
2021-09-27 12:31:14 +0300

    Determinante task second commit

--- opredelitel.rb
+++ opredelitel.rb
@@ -5,7 +5,7 @@ def Input_Matrix()
     puts "Input number of columne in matrix"
     k = gets.to_i
     if n != k
-        puts "Input non quadro matrix, try again"
+        puts "non-quadratic matrix, try again"
         puts "\n"
         matrix = Input_Matrix()
     else
@@ -24,7 +24,7 @@ def Input_Matrix()
     matrix
 end
 
-def Determinante(matrix)
+def Determinant(matrix)
     result = 0
     if matrix.length == 2
         result += (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
@@ -33,29 +33,29 @@ def Determinante(matrix)
         (0..matrix.length()-1).each do |j|# разложение матрицы по 1 строке
             matrix_j = generate_small_matrix(matrix, j)#создаем квадратичную матрицу поменьше, в которой отсутвсуют элементы 1 строки и j столбца
             if j % 2 == 0# узнаем с каким знаком нужно брать произведение на матрицу
-                result += matrix[0][j] *  Determinante(matrix_j)
+                result += matrix[0][j] *  Determinant(matrix_j)
             else
-                result -= matrix[0][j] *  Determinante(matrix_j)
+                result -= matrix[0][j] *  Determinant(matrix_j)
             end
         end
     end
     result
 end
 
-def generate_small_matrix(matrix, k)#функция гененерирущая матрциу, в которой отсутвсуют элементы 1 строки и k столбца 
+def generate_small_matrix(matrix, k)#функция создает матрциу, в которой отсутвсуют элементы 1 строки и k столбца 
     matrix_small = Array.new()
-    (1..matrix.length()-1).each do |i|#все строки кроме первой
+    (1..matrix.length()-1).each do |i|#идем по всем строкам кроме первой
         row = Array.new()
-        (0..matrix.length()-1).each do |j|#все стобцы
-            if j != k# если у нас не k столбец, то закидываем элемент в матрицу
+        (0..matrix.length()-1).each do |j|#все столбцы
+            if j != k# если у нас не k-ый столбец, то закидываем элемент в матрицу
                 row << matrix[i][j]
             end
         end
-        matrix_small << row#закидываем созданную строку в новую матрцу
+        matrix_small << row#добавляем созданную строку в новую матрицу
     end
     matrix_small
 end
 
-b = Input_Matrix()
-a = Determinante(b)
-puts a
+matrix = Input_Matrix()
+result = Determinant(matrix)
+puts result
